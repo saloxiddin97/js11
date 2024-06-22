ab03a20d29112bd58f5f5814902b627366a67d4a
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-06-22 10:11:45 +0500

    js11

---
+++ scrip.js
@@ -0,0 +1,19 @@
+//let a =12
+//console.log( typeof a);
+//let a=Math.round(Math.random()*99)
+//console.log(a);
+//if(a%2 === 0){
+    //console.log("ikiga bolinadigan son");
+
+//}
+//else{
+    //console.log("ikiga bolinmaydigan son");
+//}
+let text=prompt("soz kriting")
+let mars
+if(text.includes(mars)){
+console.log("bu soz bor");
+}
+else{
+    console.log("bu soz yoq"); 
+}
